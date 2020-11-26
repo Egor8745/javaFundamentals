5b57b97a208c7a4f19f3a9ab3dd82f1d05499760
Egor <72384263+Egor8745@users.noreply.github.com>
2020-11-26 23:50:10 +0300

    update tasks

---
+++ src/Dropbox/Dropbox2.java
@@ -0,0 +1,33 @@
+package Dropbox;
+
+
+/**
+ * Created by Lenovo on 21.11.2020.
+ */
+public class Dropbox2 {//Напишите программу, заносящую в массив первые 100 натуральных чисел, делящихся на 13 или на 17, и печатающую его
+
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        int i = 0;
+        int g = 0;
+            while (i<array.length) {
+                if (g % 13 == 0 || g % 17 == 0) {
+                    array[i] = g;
+                    System.out.println(g);
+                    i++;
+                    g++;
+
+                } else {
+                    g++;
+                }
+
+            }
+
+        }
+    }
+
+
+
+
+
+
