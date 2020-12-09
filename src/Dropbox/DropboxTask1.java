85536ee8cd12ab352694469a940e5d375c1ef8eb
Egor <72384263+Egor8745@users.noreply.github.com>
2020-12-09 18:19:49 +0300

    update tasks

--- src/Dropbox/DropboxTask1.java
+++ src/Dropbox/DropboxTask1.java
@@ -1,13 +1,22 @@
 package Dropbox;
 
+import java.util.Scanner;
+
 /**
  * Created by Lenovo on 21.11.2020.
  */
 public class DropboxTask1 {//Напишите программу, которая печатает массив сначала обычном порядке, затем в обратном
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        System.out.println("Обычный порядок массива");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Длинна массива: ");
+        int arrayLength = scan.nextInt();
+        int[] array = new int[arrayLength];
+        System.out.println("Переменные массива");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scan.nextInt();
+        }
+        System.out.println("Обычный порядок массива: ");
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
