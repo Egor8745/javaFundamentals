8c1f6715f5c933e53ace46ed8d14d628c4063a99
Egor <72384263+Egor8745@users.noreply.github.com>
2020-12-09 18:20:26 +0300

    update tasks

--- src/Dropbox/DropboxTask2.java
+++ src/Dropbox/DropboxTask2.java
@@ -6,25 +6,28 @@ package Dropbox;
  */
 public class DropboxTask2 {//Напишите программу, заносящую в массив первые 100 натуральных чисел, делящихся на 13 или на 17, и печатающую его
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Определить сумму элементов целочисленного массива,расположенных между минимальным и максимальным значениями
         int[] array = new int[100];
-        int i = 0;
         int g = 0;
-            while (i<array.length) {
-                if (g % 13 == 0 || g % 17 == 0) {
-                    array[i] = g;
-                    System.out.println(g);
-                    i++;
-                    g++;
-
-                } else {
-                    g++;
-                }
-
+        int i = 0;
+        while (i < array.length) {
+            if (g % 13 == 0 || g % 17 == 0) {
+                array[i] = g;
+                g++;
+                i++;
+            } else {
+                g++;
             }
+        }
+        printArray(array);
+    }
 
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
     }
+}
 
 
 
