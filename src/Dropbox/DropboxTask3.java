f3f1dd232c8ab3361f5ecd27ed03964e414d09ac
Egor8745 <72384263+Egor8745@users.noreply.github.com>
2021-01-02 21:03:45 +0300

    upgrade

--- src/Dropbox/DropboxTask3.java
+++ src/Dropbox/DropboxTask3.java
@@ -1,10 +1,8 @@
 package Dropbox;
 
-
 public class DropboxTask3 {
     public static void main(String[] args) {//Определить сумму элементов целочисленного массива,расположенных между минимальным и максимальным значениями
         int[] array = new int[6];
-        int sum=0;
         for (int i = 0; i < array.length; i++) {
             int random = (int) (Math.random() * 50);
             array[i] = random;
@@ -19,17 +17,31 @@ public class DropboxTask3 {
         for (int i = 1; i <= array.length - 1; i++) {
             if (max < array[i]) {
                 max = array[i];
-                indexmax=i;
+                indexmax = i;
             }
             if (min > array[i]) {
                 min = array[i];
-                indexmin=i;
+                indexmin = i;
             }
         }
-
-        System.out.println(sum);
-
-        }
-
+        int SumindexArray = sumArray(array, indexmax, indexmin);
+        System.out.println("Сумма элементов равна : " + SumindexArray);
     }
+
+    private static int sumArray(int[] array, int indexmax, int indexmin) {
+        int SumindexArray = 0;
+        int sum = 0;
+        int x;
+        if (indexmin < indexmax) {
+            x = indexmin;
+        } else {
+            x = indexmax;
+        }
+        for (int i = x; indexmin < indexmax ? i <= indexmax : i <= indexmin; i++) {
+            sum += array[i];
+            SumindexArray = sum;
+        }
+        return SumindexArray;
+    }
+}
 
